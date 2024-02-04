e587d15604d031ddc8643524bc36b51d3f5d3403
Sat727 <116710399+Sat727@users.noreply.github.com>
2024-02-04 00:02:56 -0500

    added message chunks for embeds

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ import random
 from requests import get
 from pprint import pprint
 from pathlib import Path
+import shutil
 #import threading
 #import subprocess
+
 
 
 database = sqlite3.connect("db/radio.db")
@@ -78,9 +80,7 @@ async def command(interaction: discord.Interaction, radio:str):
                     async def PickMusic():
                         s= []
                         for (dirpath, dirnames, filenames) in os.walk(path):
-                            print(filenames)
                             s.append(filenames)
-                        print(s)
                         s = random.choice([i for i in s[0]])
                         if s == client.servers[interaction.guild.id]['LastPlayed'] and len(filenames) >= 2 or s == None:
                             return await PickMusic()
@@ -94,7 +94,7 @@ async def command(interaction: discord.Interaction, radio:str):
                     if client.servers[interaction.guild.id]['VoiceData'] == None:
                         client.servers[interaction.guild.id]['VoiceData'] = player
                     ###
-                    embed = discord.Embed(title=f"{interaction.user.name}'s Radio", description=f"**Only the host can interact with the bot this session.**\n\nCurrent Song: `{client.servers[interaction.guild.id]['LastPlayed'][:-4]}`\nCurrent Radio: `{client.servers[interaction.guild.id]['Radio']}`")
+                    embed = discord.Embed(title=f"{interaction.user.name}'s Radio", description=f"**Only the host can interact with the bot this session.**\n\nCurrent Song: `{client.servers[interaction.guild.id]['LastPlayed'][:-4].replace('_',' ')}`\nCurrent Radio: `{client.servers[interaction.guild.id]['Radio']}`")
                     if message == None:
                         message = await interaction.original_response()
                         message = await client.get_channel(interaction.channel.id).fetch_message(message.id)
@@ -106,7 +106,7 @@ async def command(interaction: discord.Interaction, radio:str):
                         await player.disconnect(), cleandata()
                         return
                     await asyncio.sleep(3)
-            #except Exception as e:
+            #    except Exception as e:
             #    await player.disconnect(), cleandata()
             #    print("Unknown error, crashed"), print(e), cleandata()
             #    return False
@@ -127,7 +127,7 @@ async def skip(interaction:discord.Interaction):
         await interaction.response.send_message("No song playing!")
     if client.servers[interaction.guild.id]['Host'] == interaction.user.id:
         client.servers[interaction.guild.id]['VoiceData'].stop()
-        await interaction.response.send_message(f"Skipped {client.servers[interaction.guild.id]['LastPlayed'][:-3]}") #.split('-')[1:][0].replace('_','')}")
+        await interaction.response.send_message(f"Skipped {client.servers[interaction.guild.id]['LastPlayed'][:-4].replace('_',' ')}") #.split('-')[1:][0].replace('_','')}")
     else:
         await interaction.response.send_message("You are not the host")
 
@@ -171,24 +171,26 @@ async def create(interaction: discord.Interaction, name:str, private:bool):
         await interaction.response.send_message("Invalid name for radio")
 
 @app_commands.choices(action=[
-        app_commands.Choice(name="Add", value=""),
-        app_commands.Choice(name="Remove", value=""),
+        app_commands.Choice(name="Add", value="add"),
+        app_commands.Choice(name="Remove", value="remove"),
         ])
-@tree.command(name='radio', description='Add/Remove song from radio')
-async def radio(interaction: discord.Interaction, radio:str, action:app_commands.Choice[str], song:str):
+@tree.command(name='song', description='Add/Remove song from radio')
+async def radio(interaction: discord.Interaction, radio:str, action:app_commands.Choice[str], song:str|None):
         data = db.execute("SELECT * FROM radios").fetchall()
         if not Path.exists(Path(f'./{interaction.user.id}/{radio}')) or radio not in [i[1] for i in data if i[0] == interaction.user.id]:
             await interaction.response.send_message("That radio does not exist, or you do not own it")
         else:
             if action.name == 'Add':
-
+                if song == None:
+                    await interaction.response.send_message("Not a valid song, please add a **'song' parameter**\n*/song radioname Add songname*")
+                    return
                 await interaction.response.defer()
                 class ButtonView(discord.ui.View):
                     def __init__(self):
                         super().__init__(timeout=None)
                 b = ButtonView()
                 async def ytfunction(arg:str, download=False, interaction=interaction, ):
-                    YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True', 'outtmpl': f'./{interaction.user.id}/{radio}/%(title)s.%(ext)s',
+                    YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True', 'outtmpl': f'./{interaction.user.id}/{radio}/%(title)s.%(ext)s', 'restrictfilenames': True,
   "forceurl": True,
                    'postprocessors': [{
             'key': 'FFmpegExtractAudio',
@@ -211,14 +213,17 @@ async def radio(interaction: discord.Interaction, radio:str, action:app_commands
                             c=1
                             for i in video:
                                 if i['duration'] <= 330 and i['duration'] >= 61:
-                                    async def downloadmusic(interaction):
-                                        b.stop()
-                                        await interaction.response.defer()
-                                        await interaction.message.edit(view=None)
-                                        response = await asyncrequest((str(list(entries[int(list(interaction.data.values())[0])-1].values())[0][0])))
-                                        message = await interaction.original_response()
-                                        message = await client.get_channel(interaction.channel.id).fetch_message(message.id)
-                                        await message.edit(content=f"Completed the request to download {response['title']} to {radio}, {interaction.user.mention}",embed=None)
+                                    async def downloadmusic(buttoninteraction):
+                                        if interaction.user.id == buttoninteraction.user.id:
+                                            b.stop()
+                                            await buttoninteraction.response.defer()
+                                            await buttoninteraction.message.edit(view=None)
+                                            response = await asyncrequest((str(list(entries[int(list(buttoninteraction.data.values())[0])-1].values())[0][0])))
+                                            message = await interaction.original_response()
+                                            message = await client.get_channel(buttoninteraction.channel.id).fetch_message(message.id)
+                                            await message.edit(content=f"Completed the request to download {response['title']} to {radio}, {interaction.user.mention}",embed=None)
+                                        else:
+                                            await buttoninteraction.response.send_message("You do not have permission", ephemeral=True)
                                     button = discord.ui.Button()
                                     button.label = c
                                     button.custom_id = str(c)
@@ -230,31 +235,58 @@ async def radio(interaction: discord.Interaction, radio:str, action:app_commands
                             response = await asyncrequest(arg)
                             #message = await interaction.original_response()
                             #message = await client.get_channel(interaction.channel.id).fetch_message(message.id)
-                            await interaction.followup.send(content=f"Completed the request to download {response['title']} to {radio}, {interaction.user.mention}",embed=None)
+                            await interaction.followup.send(content=f"Completed the request to download {response['title'].replace('_', '')} to {radio}, {interaction.user.mention}",embed=None)
                             #video = ydl.download(arg)
                             return False
                     return entries
 
-            entries = await ytfunction(arg=song)
-            embed = discord.Embed(title="Search Results",description="Up to 3 top searches from the internet, please choose one to add to your radio", color=0x1B00FF)
-            c = 1
-            if entries:
-                for i in entries:
-                    for key, value in i.items():
-                        m, s = divmod(value[1], 60)
-                        h, m = divmod(m, 60)
-                        embed.add_field(name=f"{c}. {str(key[:50])}" if len(key) >= 30 else str(key), value=f'{f"{m}m" if m > 0 else ""} {f"{s}s" if s > 0 else ""}', inline=False)
-                        c+=1
-            print(entries)
-            if entries != False and type(entries) != list:
-                await interaction.followup.send("No eligible search results found. All result entries may exceed 5.5 minutes")
-            elif entries == False:
-                return
-            else:
-                await interaction.followup.send(embed=embed,view=b)
+                entries = await ytfunction(arg=song)
+                embed = discord.Embed(title="Search Results",description="Up to 3 top searches from the internet, please choose one to add to your radio", color=0x1B00FF)
+                c = 1
+                if entries:
+                    for i in entries:
+                        for key, value in i.items():
+                            m, s = divmod(value[1], 60)
+                            h, m = divmod(m, 60)
+                            embed.add_field(name=f"{c}. {str(key[:50])}" if len(key) >= 30 else str(key), value=f'{f"{m}m" if m > 0 else ""} {f"{s}s" if s > 0 else ""}', inline=False)
+                            c+=1
+                print(entries)
+                if entries != False and type(entries) != list:
+                    await interaction.followup.send("No eligible search results found. All result entries may exceed 5.5 minutes")
+                elif entries == False:
+                    return
+                else:
+                    await interaction.followup.send(embed=embed,view=b)
+            if action.name == 'Remove':
+                path = f"./{interaction.user.id}/{radio}"
+                s = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51]
+                for (dirpath, dirnames, filenames) in os.walk(path):
+                    s.append(filenames)
+                index = 0
+                #class IterableEmbed():
+                #    def __init__(self):
+                #        super().__init__(command_prefix='!', intents=discord.Intents.all())
+                #        self.synced = False
+                #        self.servers = {}
+                #        self.bot = discord
+                #        self.index = 0
+                #        self.chunks = [s[i:i + 25] for i in range(0, len(s), 25)]
+                async def changeindex(self, buttoninteraction, value, index):
+                    if interaction.user.id == buttoninteraction.user.id:
+                        if value == '0':
+                            index -= 1
+                    print(changeindex())
+                    #data = db.execute("SELECT * FROM radios").fetchall()
+                    #if not Path.exists(Path(f'./{interaction.user.id}/{radio}')) or radio not in [i[1] for i in data if i[0] == interaction.user.id]:
+                    #    await interaction.response.send_message("That radio does not exist, or you do not own it")
+                    #else:
+                    #    db.execute("DELETE FROM radios WHERE radio = ? ", (radio))
+                    #    shutil.rmtree(f'./{interaction.user.id}/{radio}')
+                    embed = discord.Embed(f"Listed songs for {radio}")
+                await interaction.response.send_message("")
 
 
-
+range
 
 #print(search('test'))
 client.run('')
